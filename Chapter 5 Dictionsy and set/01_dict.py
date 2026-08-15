60c30de8916f66b15243c1f64b8c54ddb657507a
Sujal Dilip Umate <ps5untilgtavi@gmail.com>
2026-08-15 15:01:53 +0530

    Add dictonaries and Sets in Python

---
+++ Chapter 5 Dictionsy and set/01_dict.py
@@ -0,0 +1,8 @@
+marks = {
+    "Harry":100,
+    "SHubham":56,
+    "Rohan":23
+}
+
+# print(marks, type{marks})
+print(marks["Harry"])
---
+++ Chapter 5 Dictionsy and set/02_dict_method.py
@@ -0,0 +1,10 @@
+marks = {
+    "Harry":100,
+    "SHubham":56,
+    "Rohan":23
+}
+
+
+print(marks.items())
+print(marks.keys())
+print(marks.values())
